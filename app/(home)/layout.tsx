7574c370cc4e2677f7ca4cbc11bb4daf189bf77e
Amol derick soans <amoldericksoans@gmail.com>
2025-09-24 00:01:15 +0530

    Add core application layout and page structure

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,14 @@
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/lib/layout.shared';
+import { baseOptions, linkItems } from '@/lib/layout.shared';
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+  return (
+    <HomeLayout
+      {...baseOptions()}
+      links={linkItems}
+      className="helios"
+    >
+      {children}
+    </HomeLayout>
+  );
 }
